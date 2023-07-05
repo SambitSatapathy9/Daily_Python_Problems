31a815afc7cc6a765507347fadcb4c888211b180
Sambit K Satapathy <123438716+SambitSatapathy9@users.noreply.github.com>
2023-07-06 00:05:19 +0530

    Update Problem_09_05-07-23.py

--- Problem_09_05-07-23.py
+++ Problem_09_05-07-23.py
@@ -1,4 +1,23 @@
 #Problem 9 (05-07-2023)
+#Problem Statement: Give an example of try...except...else....finally block
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+except ZeroDivisionError:
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+else:
+    print("success a=",a)
+finally:
+    print("Processing Complete")
+
+
+#Problem 10 
 #Problem Statement: Logistic Regression
 #Say you have 6 input variables, and corresponding 6 output. The output can be either 0 or 1. Create 
 # a function plot_data and plot the corresponding data. 
