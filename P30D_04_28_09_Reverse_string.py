7a243c2ce77e592123262699f0e6ba1e05361c4a
Sambit K Satapathy <123438716+SambitSatapathy9@users.noreply.github.com>
2023-09-28 12:30:58 +0530

    Create P30D_04_28_09_Reverse_string.py

---
+++ P30D_04_28_09_Reverse_string.py
@@ -0,0 +1,8 @@
+#30 Days Coding Challenge
+#Q2 - Reverse a String 
+
+def reverse_string(string):
+    return string[::-1]
+
+a = "Abhi na jaao chor kar ke dil abhi bhara nahi!"
+print(reverse_string(a))
