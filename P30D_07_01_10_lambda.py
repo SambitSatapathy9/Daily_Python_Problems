26969afe047ddc4a941a3e761ffe44f10f0b60e6
Sambit K Satapathy <123438716+SambitSatapathy9@users.noreply.github.com>
2023-10-01 14:37:42 +0530

    Create P30D_07_01_10_lambda.py

---
+++ P30D_07_01_10_lambda.py
@@ -0,0 +1,6 @@
+#30 Days Coding Challenge 29-09-23
+#Q7 - Write a Python program to find whether a given string starts with a given character using Lambda.
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Python is a programming language'))
+print(starts_with('Java is a programming language'))
+
